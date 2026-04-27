407847046ad841117e5a8b2f6337ac10b4724744
davialmeidagoncalves4-hue <davialmeidagoncalves4@gmail.com>
2026-04-27 16:04:37 +0000

    versão 1

--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,6 @@
-
+#Exemplo de uso da Variavel Semtinela
+while True:
+ comando = input ("digite um comando-paraparar digite "sair"")
+ if comando == "sair":
+   break
+print (f"Executando: {comando}")
