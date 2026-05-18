0d794c6b7579bf0796a5e6a7a8697510fb03572d
davialmeidagoncalves4-hue <davialmeidagoncalves4@gmail.com>
2026-05-18 14:20:18 +0000

    v1

---
+++ investimento.py
@@ -0,0 +1,6 @@
+deposito = 50
+total = 0
+for mes in range(1, 7):
+    total = total + deposito
+    print (f"Mês:{mes} ,saldo tootal:R${total}")
+print(f"ao final do {mes} meses, voce tera R${total}")
---
+++ tabuada.py
@@ -0,0 +1,6 @@
+numero = int(input("tabuada de qual numero? "))
+print(f"___ tabuada do {numero} ---")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"(nummero) x {i} = {resultado}")  
+  
